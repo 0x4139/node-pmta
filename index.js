b1b41c8e342fe8f4f8284111b9331c4cc326973a
Vali Malinoiu <vali@Valis-MacBook-Pro.local>
2015-08-11 18:07:10 +0300

    updates the connection object

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ var payload = [
     "MIME-Version: 1.0",
     "Content-Type: text/plain; charset=utf-8",
     "Content-Transfer-Encoding: 7bit",
-    '',
+    '\n',
     "This is a message, [fname]"
 ].join("\n");
 
 
 let recipient = new PMTA.Recipient('kodeslacker@yahoo.com');
 recipient.defineVariable('fname','FnameWasSet');
-recipient.setFrom('Vali <vali@nasa.com>');
 let message = new PMTA.Message('test@nasa.com');
 message.addRecipient(recipient);
 message.setPayload(payload);
-
 console.log(message.serialize());
+let connection = new PMTA.Connection('77.36.17.85',2225);
+connection.submit(message);
