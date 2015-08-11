491707a3d3521bc94432328dbe2eeb746bb2f63e
Vali Malinoiu <vali@Valis-MacBook-Pro.local>
2015-08-11 12:57:08 +0300

    adds the basic backbone for the connection and commits the message definition

--- index.js
+++ index.js
@@ -2,7 +2,23 @@
 
 var PMTA = require('./lib/PMTA');
 
-let recipient = new PMTA.Recipient('kodeslacker@yahoo.com');
-recipient.defineVariable('foo','bar');
-console.log(recipient.serialize());
+var payload = [
+    "From: [*from]",
+    "To: <[*to]>",
+    "Subject: PMTA message",
+    "MIME-Version: 1.0",
+    "Content-Type: text/plain; charset=utf-8",
+    "Content-Transfer-Encoding: 7bit",
+    '',
+    "This is a message, [fname]"
+].join("\n");
 
+
+let recipient = new PMTA.Recipient('kodeslacker@yahoo.com');
+recipient.defineVariable('fname','FnameWasSet');
+recipient.setFrom('Vali <vali@nasa.com>');
+let message = new PMTA.Message('test@nasa.com');
+message.addRecipient(recipient);
+message.setPayload(payload);
+
+console.log(message.serialize());
